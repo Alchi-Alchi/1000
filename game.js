3f835671472a52a609c600e32cc0550ddc4401f0
PawelWlasow <50367039+PawelWlasow@users.noreply.github.com>
2021-12-26 21:21:48 +0300

    svgDrop

--- game.js
+++ game.js
@@ -4,10 +4,13 @@ let c3 = document.getElementById ('c3');
 let c4 = document.getElementById ('c4');
 let c5 = document.getElementById ('c5');
 function getNumber(min, max) {
-  c1.innerHTML = Math.floor (Math.random () * (max - min + 1)) + min;
-  c2.innerHTML = Math.floor (Math.random () * (max - min + 1)) + min;
-  c3.innerHTML = Math.floor (Math.random () * (max - min + 1)) + min;
-  c4.innerHTML = Math.floor (Math.random () * (max - min + 1)) + min;
-  c5.innerHTML = Math.floor (Math.random () * (max - min + 1)) + min;
-  console.log (c1.innerText)
+  let n1 = Math.floor (Math.random () * (max - min + 1)) + min;
+  let n2 = Math.floor (Math.random () * (max - min + 1)) + min;
+  let n3 = Math.floor (Math.random () * (max - min + 1)) + min;
+  let n4 = Math.floor (Math.random () * (max - min + 1)) + min;
+  let n5 = Math.floor (Math.random () * (max - min + 1)) + min;
+  console.log (n1)
+  if (n1 == 1) {
+    c1.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=50 cy=50 r=10 stroke=black fill=black /></svg>"
+  }
 }
