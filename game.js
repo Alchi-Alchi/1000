3c635948e70351c4e5767f5936832a0f6ac499cc
PawelWlasow <50367039+PawelWlasow@users.noreply.github.com>
2021-12-28 17:44:51 +0300

    drops

--- game.js
+++ game.js
@@ -1,127 +1,54 @@
-let c1 = document.getElementById ('c1');
-let c2 = document.getElementById ('c2');
-let c3 = document.getElementById ('c3');
-let c4 = document.getElementById ('c4');
-let c5 = document.getElementById ('c5');
-let svg;
-function getNumber(min, max) {
-  let n1 = Math.floor (Math.random () * (max - min + 1)) + min;
-  let n2 = Math.floor (Math.random () * (max - min + 1)) + min;
-  let n3 = Math.floor (Math.random () * (max - min + 1)) + min;
-  let n4 = Math.floor (Math.random () * (max - min + 1)) + min;
-  let n5 = Math.floor (Math.random () * (max - min + 1)) + min;
-  // function createDropOne (cube) {
-  //   let svg = document.createElement ('svg');
-  //   svg.setAttribute ('width', '100');
-  //   svg.setAttribute ('height', '100');
-  //   svg.setAttribute ('xmlns', 'http://www.w3.org/2000/svg');
-  //   let circle = document.createElement ('circle');
-  //   circle.setAttribute ('cx','50');
-  //   circle.setAttribute ('cy','50');
-  //   circle.setAttribute ('r','10');
-  //   circle.setAttribute ('stroke','black');
-  //   circle.setAttribute ('fill','black');
-  //   svg.appendChild (circle);
-    // cube.innerHTML = svg;
-  // }
-  switch (n1) {
+function getSVG(number, cube) {
+  switch (number) {
     case 1:
-    c1.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=50 cy=50 r=10 stroke=black fill=black /></svg>"
-    break;
-    case 2: 
-    c1.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>"
-    break;
-    case 3: 
-    c1.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=50 cy=50 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>"
-    break;
-    case 4: 
-    c1.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=85 cy=15 r=10 stroke=black fill=black /><circle cx=15 cy=85 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>"
-    break;
-    case 5: 
-    c1.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=85 cy=15 r=10 stroke=black fill=black /><circle cx=50 cy=50 r=10 stroke=black fill=black /><circle cx=15 cy=85 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>"
-    break;
-    case 6: 
-    c1.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=85 cy=15 r=10 stroke=black fill=black /><circle cx=15 cy=50 r=10 stroke=black fill=black /><circle cx=85 cy=50 r=10 stroke=black fill=black /><circle cx=15 cy=85 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>"
-    break;
-  }
-  switch (n2) {
-    case 1: 
-    c2.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=50 cy=50 r=10 stroke=black fill=black /></svg>"
-    break;
-    case 2: 
-    c2.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>"
-    break;
-    case 3: 
-    c2.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=50 cy=50 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>"
-    break;
-    case 4: 
-    c2.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=85 cy=15 r=10 stroke=black fill=black /><circle cx=15 cy=85 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>"
-    break;
-    case 5: 
-    c2.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=85 cy=15 r=10 stroke=black fill=black /><circle cx=50 cy=50 r=10 stroke=black fill=black /><circle cx=15 cy=85 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>"
-    break;
-    case 6: 
-    c2.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=85 cy=15 r=10 stroke=black fill=black /><circle cx=15 cy=50 r=10 stroke=black fill=black /><circle cx=85 cy=50 r=10 stroke=black fill=black /><circle cx=15 cy=85 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>"
-    break;
-  }
-  switch (n3) {
-    case 1: 
-    c3.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=50 cy=50 r=10 stroke=black fill=black /></svg>"
-    break;
-    case 2: 
-    c3.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>"
-    break;
-    case 3: 
-    c3.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=50 cy=50 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>"
-    break;
-    case 4: 
-    c3.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=85 cy=15 r=10 stroke=black fill=black /><circle cx=15 cy=85 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>"
-    break;
-    case 5: 
-    c3.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=85 cy=15 r=10 stroke=black fill=black /><circle cx=50 cy=50 r=10 stroke=black fill=black /><circle cx=15 cy=85 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>"
-    break;
-    case 6: 
-    c3.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=85 cy=15 r=10 stroke=black fill=black /><circle cx=15 cy=50 r=10 stroke=black fill=black /><circle cx=85 cy=50 r=10 stroke=black fill=black /><circle cx=15 cy=85 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>"
-    break;
-  }
-  switch (n4) {
-    case 1: 
-    c4.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=50 cy=50 r=10 stroke=black fill=black /></svg>"
-    break;
-    case 2: 
-    c4.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>"
-    break;
-    case 3: 
-    c4.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=50 cy=50 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>"
-    break;
-    case 4: 
-    c4.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=85 cy=15 r=10 stroke=black fill=black /><circle cx=15 cy=85 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>"
-    break;
-    case 5: 
-    c4.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=85 cy=15 r=10 stroke=black fill=black /><circle cx=50 cy=50 r=10 stroke=black fill=black /><circle cx=15 cy=85 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>"
-    break;
-    case 6: 
-    c4.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=85 cy=15 r=10 stroke=black fill=black /><circle cx=15 cy=50 r=10 stroke=black fill=black /><circle cx=85 cy=50 r=10 stroke=black fill=black /><circle cx=15 cy=85 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>"
-    break;
-  }
-  switch (n5) {
-    case 1: 
-    c5.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=50 cy=50 r=10 stroke=black fill=black /></svg>"
-    break;
-    case 2: 
-    c5.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>"
-    break;
-    case 3: 
-    c5.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=50 cy=50 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>"
-    break;
-    case 4: 
-    c5.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=85 cy=15 r=10 stroke=black fill=black /><circle cx=15 cy=85 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>"
-    break;
-    case 5: 
-    c5.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=85 cy=15 r=10 stroke=black fill=black /><circle cx=50 cy=50 r=10 stroke=black fill=black /><circle cx=15 cy=85 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>"
-    break;
-    case 6: 
-    c5.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=85 cy=15 r=10 stroke=black fill=black /><circle cx=15 cy=50 r=10 stroke=black fill=black /><circle cx=85 cy=50 r=10 stroke=black fill=black /><circle cx=15 cy=85 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>"
-    break;
+      cube.innerHTML = "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=50 cy=50 r=10 stroke=black fill=black /></svg>";
+      break;
+    case 2:
+      cube.innerHTML =
+        "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>";
+      break;
+    case 3:
+      cube.innerHTML =
+        "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=50 cy=50 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>";
+      break;
+    case 4:
+      cube.innerHTML =
+        "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=85 cy=15 r=10 stroke=black fill=black /><circle cx=15 cy=85 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>";
+      break;
+    case 5:
+      cube.innerHTML =
+        "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=85 cy=15 r=10 stroke=black fill=black /><circle cx=50 cy=50 r=10 stroke=black fill=black /><circle cx=15 cy=85 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>";
+      break;
+    case 6:
+      cube.innerHTML =
+        "<svg width = 100 height = 100 xmlns = 'http://www.w3.org/2000/svg'><circle cx=15 cy=15 r=10 stroke=black fill=black /><circle cx=85 cy=15 r=10 stroke=black fill=black /><circle cx=15 cy=50 r=10 stroke=black fill=black /><circle cx=85 cy=50 r=10 stroke=black fill=black /><circle cx=15 cy=85 r=10 stroke=black fill=black /><circle cx=85 cy=85 r=10 stroke=black fill=black /></svg>";
+      break;
   }
 }
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getNumber(min, max) {
+  let c1 = document.getElementById("c1");
+  let c2 = document.getElementById("c2");
+  let c3 = document.getElementById("c3");
+  let c4 = document.getElementById("c4");
+  let c5 = document.getElementById("c5");
+
+  let n1 = getRandom(min, max);
+  let n2 = getRandom(min, max);
+  let n3 = getRandom(min, max);
+  let n4 = getRandom(min, max);
+  let n5 = getRandom(min, max);
+
+  getSVG(n1, c1);
+  getSVG(n2, c2);
+  getSVG(n3, c3);
+  getSVG(n4, c4);
+  getSVG(n5, c5);
+}
+
+function playersAdd () {};
+function scores () {};
